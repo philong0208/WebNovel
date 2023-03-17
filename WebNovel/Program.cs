efab97938549c742529d97cef52c13440b020201
n18dccn106@student.ptithcm.edu.vn <n18dccn106@student.ptithcm.edu.vn>
2023-03-17 21:13:49 +0700

    Revert "Rebuild solution"
    This reverts commit 52e438dc4082838b8b55eee5159e7ba51a20ef3c.

--- WebNovel/Program.cs
+++ WebNovel/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using WebNovel.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
+using WebNovel.Repositories.Abstract;
+using WebNovel.Repositories.Implementation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,11 @@ builder.Services.AddAuthentication(
         option.LoginPath = "/Access/Login";
         option.ExpireTimeSpan = TimeSpan.FromMinutes(5);
     });
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//    .AddUserStore<WebNovelContext>()
+//    .AddDefaultTokenProviders();
+//builder.Services.ConfigureApplicationCookie(options => options.LoginPath = "/Access/Login");
+//builder.Services.AddScoped<IUserAuthenticationService, UserAuthenticationService>();
 
 var app = builder.Build();
 
