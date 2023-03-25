499a28afc902bd086f777af82b021f417cdd96c6
n18dccn106@student.ptithcm.edu.vn <n18dccn106@student.ptithcm.edu.vn>
2023-03-25 09:52:58 +0700

    Add Pagination to Home page

--- WebNovel/Controllers/HomeController.cs
+++ WebNovel/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using WebNovel.Models;
+using WebNovel.Models.Pagination;
 
 namespace WebNovel.Controllers
 {
@@ -9,15 +11,45 @@ namespace WebNovel.Controllers
         private readonly WebNovelContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(WebNovelContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string searchText = "", int pg = 1, int pageSize = 5)
         {
+            var webNovelContext = _context.Novels.Include(n => n.Author).Include(n => n.Genres).AsQueryable();
+
+            //List<Novel> novels = _context.Novels.ToList();
+
+            //const int pageSize = 5;
+            if (pg < 1) { 
+                pg = 1; 
+            }
+
+            if (searchText != "" && searchText != null)
+            {
+                webNovelContext = _context.Novels.Include(n => n.Author).Include(n => n.Genres)
+                    .Where(p => p.NovelTitle.Contains(searchText))
+                    .AsQueryable();
+            }
+
+            int recordCount = webNovelContext.Count();
+
+            var pager = new Pager(recordCount, pg, pageSize);
+
+            int recordSkip = (pg - 1) * pageSize;
+
+            List<Novel> data = await webNovelContext.Skip(recordSkip).Take(pager.PageSize).ToListAsync();
+
+            //List<Novel> returnListNovel = await webNovelContext.Skip(recordSkip).Take(pageSize).ToListAsync();
             
-            return View();
+            this.ViewBag.Pager = pager;
+
+
+            //return View(await webNovelContext.ToListAsync());
+
+            return View(data);
         }
 
         public IActionResult Privacy()
@@ -30,11 +62,5 @@ namespace WebNovel.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        //public IEnumerable<Novel> getListNovel()
-        //{
-        //    var list = new List<Novel>();
-        //    return list;
-        //}
     }
 }
